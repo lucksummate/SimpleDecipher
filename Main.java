107f4c964d6c5836a33ac12f92a8c4ec25b5accc
lucksummate <152327737+lucksummate@users.noreply.github.com>
2024-02-01 14:17:06 -0500

    Add files via upload
    Fixed the save feature
    + added a percentage calc.

--- Main.java
+++ Main.java
@@ -11,6 +11,7 @@ public class Main {
         while (true) {
             System.out.println("Encoded Message (Type 'exit' to quit):");
             String encoded = scanner.nextLine();
+            swapHistory.add(encoded);
 
             if (encoded.equalsIgnoreCase("exit")) {
                 break;
@@ -35,29 +36,33 @@ public class Main {
 
         for (int i = 0; i < charCount.length; i++) {
             if (charCount[i] > 0) {
-                System.out.println((char) ('a' + i) + " " + charCount[i]);
+                float n = 26;
+                Float percentage = (charCount[i]/n) * 100;
+                System.out.println((char) ('a' + i) + " " + percentage + "%");
             }
         }
-      while (true){
-        System.out.println("Enter the letter you want to replace (or 'undo' to undo the last swap): ");
-        String input = scanner.nextLine().toLowerCase();
 
-        if (input.equals("undo")) {
-            undoLastSwap();
-            return;
+        while (true) {
+            System.out.println("Enter the letter you want to replace (or 'undo' to undo the last swap): ");
+            String input = scanner.nextLine().toLowerCase();
+
+            if (input.equals("undo")) {
+                undoLastSwap();
+                return;
+            }
+            if (input.equals("exit")) {
+                System.exit(0);
+            }
+
+            char letterToReplace = input.charAt(0);
+
+            System.out.println("Enter the character to replace with: ");
+            char replacementChar = scanner.nextLine().charAt(0);
+
+            String swapped = swapSpecificLetter(swapHistory.get(swapHistory.size() - 1), letterToReplace, replacementChar);
+            saveSwap(swapped);
+            System.out.println("Swapped Message: " + swapped);
         }
-        if (input.equals("Quit")){
-          break;
-        }
-        char letterToReplace = input.charAt(0);
-
-        System.out.println("Enter the character to replace with: ");
-        char replacementChar = scanner.nextLine().charAt(0);
-
-        String swapped = swapSpecificLetter(encoded, letterToReplace, replacementChar);
-        saveSwap(swapped);
-        System.out.println("Swapped Message: " + swapped);
-      }
     }
 
     public static String swapSpecificLetter(String input, char letterToReplace, char replacementChar) {
